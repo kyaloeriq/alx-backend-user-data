af059350ea5b734f779c67745452b618c54c85a3
kyaloeriq <kyaloeriq@gmail.com>
2024-08-30 00:59:00 -0700

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,7 +19,7 @@ def filter_datum(
     """
     pattern = '|'.join([
         f'(?<={field}={separator})[^{separator}]+' for field in fields
-        ])
+    ])
     return re.sub(pattern, redaction, message)
 
 
@@ -30,7 +30,6 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-
     def __init__(self, fields: List[str]):
         """
         Initialize the formatter with the list of fields to redact.
@@ -38,14 +37,12 @@ class RedactingFormatter(logging.Formatter):
         super().__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         """
         Format the log record and filter sensitive fields.
         """
         # Format the log record using the superclass's format method
         formatted_message = super().format(record)
-        
         # Apply filtering to the formatted message
         return filter_datum(
             self.fields, self.REDACTION, formatted_message, self.SEPARATOR
@@ -54,22 +51,6 @@ class RedactingFormatter(logging.Formatter):
 
 def get_logger() -> logging.Logger:
     """
-    Creates and configures a logger named 'user_data' that logs up to INFO level
+    Creates and configures a logger named 'user_data'
     """
     # Create a logger with the name 'user_data'
-    logger = logging.getLogger("user_data")
-    
-    # Set log level to INFO
-    logger.setLevel(logging.INFO)
-    
-    # Ensure the logger does not propagate messages to other loggers
-    logger.propagate = False
-    
-    # Create a StreamHandler and set the RedactingFormatter as the formatter
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    
-    # Add the handler to the logger
-    logger.addHandler(stream_handler)
-    
-    return logger
