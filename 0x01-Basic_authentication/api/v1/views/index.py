56a736a79437a705fcd828169ea6b584230b400d
kyaloeriq <kyaloeriq@gmail.com>
2024-09-03 08:29:45 +0000

    index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,7 @@ from flask import Blueprint, abort
 
 index_bp = Blueprint('index', __name__)
 
+
 @index_bp.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized():
     """Endpoint that raises a 401 Unauthorized error."""
