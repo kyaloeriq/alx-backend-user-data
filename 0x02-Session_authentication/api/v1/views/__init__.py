54a33f432b1b4973ab242660849fa0370b5b9c38
kyaloeriq <kyaloeriq@gmail.com>
2024-09-07 17:24:50 +0000

    __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,1 +1,10 @@
-# This file is intentionally left empty
+#!/usr/bin/env python3
+"""Initialization of views package"""
+
+from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Import all view modules here
+from api.v1.views.index import *
+from api.v1.views.users import *
