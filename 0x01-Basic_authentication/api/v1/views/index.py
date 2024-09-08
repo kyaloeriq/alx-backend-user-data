789fe03190c5ac3217fbdf7a283bec0818833d95
kyaloeriq <kyaloeriq@gmail.com>
2024-09-08 19:11:35 +0000

    index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,12 +7,14 @@ from flask import Blueprint, jsonify, abort
 # Create a Blueprint instance for the index routes
 index_bp = Blueprint('index', __name__)
 
+
 @index_bp.route('/', methods=['GET'])
 def index():
     """
     Route returns a JSON response with a simple "OK" message
     """
     return jsonify({"message": "OK"}), 200
+
 
 @index_bp.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized():
