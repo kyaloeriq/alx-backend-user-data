f56fc63b47acba4428049b0ff9a563185278f79b
kyaloeriq <kyaloeriq@gmail.com>
2024-09-08 18:50:10 +0000

    index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 # api/v1/views/index.py
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort
 
 index_bp = Blueprint('index', __name__)
 
@@ -7,3 +7,8 @@ index_bp = Blueprint('index', __name__)
 def index():
     """Base route."""
     return jsonify({"message": "OK"}), 200
+
+@index_bp.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    """Endpoint to trigger 401 Unauthorized error."""
+    abort(401)
