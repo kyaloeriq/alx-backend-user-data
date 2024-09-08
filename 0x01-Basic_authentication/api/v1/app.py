195c86b6ec56150df8c1d49afc324935b6da3307
kyaloeriq <kyaloeriq@gmail.com>
2024-09-08 18:46:26 +0000

    #!/usr/bin/env python3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # api/v1/app.py
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from api.v1.views.index import index_bp
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Register blueprints
 app.register_blueprint(index_bp)
 
-
+# Error handler for 401 Unauthorized
 @app.errorhandler(401)
 def unauthorized_error(error):
     """Handle 401 Unauthorized errors."""
@@ -17,10 +17,10 @@ def unauthorized_error(error):
     response.status_code = 401
     return response
 
-
 # Other app configurations and routes
 if __name__ == '__main__':
     import os
     host = os.getenv('API_HOST', '0.0.0.0')
     port = int(os.getenv('API_PORT', 5000))
     app.run(host=host, port=port)
+
