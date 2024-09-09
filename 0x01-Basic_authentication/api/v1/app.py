d343d6ad3ef75753719bd872abe1680131314346
kyaloeriq <kyaloeriq@gmail.com>
2024-09-09 18:09:15 +0000

    validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 This module sets up the Flask application for the API, including configuration
 """
 
+from api.v1.views.index import index_bp
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 import os
@@ -11,8 +12,6 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# Import blueprints
-from api.v1.views.index import index_bp
 app.register_blueprint(index_bp)
 
 # Initialize auth to None
@@ -24,26 +23,27 @@ if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request_handler():
     """Handler for filtering requests before they are processed."""
     if auth is None:
         return
-    
     # List of paths that do not require authentication
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+            ]
 
     # If the request path does not require authentication, do nothing
     if not auth.require_auth(request.path, excluded_paths):
         return
-    
     # If the request does not contain the Authorization header, raise 401 Unauthorized
     if auth.authorization_header(request) is None:
         abort(401)
-    
     # If the current user is not found, raise 403 Forbidden
     if auth.current_user(request) is None:
         abort(403)
+
 
 @app.errorhandler(401)
 def unauthorized_error(error):
@@ -52,12 +52,14 @@ def unauthorized_error(error):
     response.status_code = 401
     return response
 
+
 @app.errorhandler(403)
 def forbidden_error(error):
     """Handle 403 Forbidden errors."""
     response = jsonify({"error": "Forbidden"})
     response.status_code = 403
     return response
+
 
 # Other app configurations and routes
 if __name__ == '__main__':
