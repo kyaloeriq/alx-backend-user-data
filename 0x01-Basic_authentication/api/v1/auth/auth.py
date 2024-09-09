d643137ef9e3420ee092068e2836caa51a606a3a
kyaloeriq <kyaloeriq@gmail.com>
2024-09-09 17:49:28 +0000

    method def require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,20 @@ class Auth:
         """
         Determine if a path requires authentication.
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Normalize path to ensure it ends with a slash
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if the normalized path is in the excluded paths
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
