af76a2aa57d9f5699195f4abeef2864a9d22298d
kyaloeriq <kyaloeriq@gmail.com>
2024-09-12 16:23:12 +0000

    a SQLAlchemy model named User for a database table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,11 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     User model mapped to the 'users' table in the database
     """
-    
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
@@ -20,5 +20,3 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
-
-# After defining the model, you can use Base.metadata.create_all(engine) to create the table in the database.
