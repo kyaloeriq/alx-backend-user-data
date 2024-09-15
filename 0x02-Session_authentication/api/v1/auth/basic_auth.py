f89f918b69e0673a9322845dff6242ba2b3d0b46
kyaloeriq <kyaloeriq@gmail.com>
2024-09-15 14:35:33 +0000

    current_user

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -98,33 +98,16 @@ class BasicAuth(Auth):
 
         return user
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Retrieves the User instance for the request based
-        """
-        if request is None:
-            return None
-
+    def current_user(self, request=None):
+        """Retrieve the user based on the Authorization header"""
         auth_header = self.authorization_header(request)
         if auth_header is None:
             return None
-
-        base64_auth_header = self.extract_base64_authorization_header(
-                auth_header
-                )
-        if base64_auth_header is None:
+        user_credentials = self.extract_base64_authorization_header(auth_header)
+        if user_credentials is None:
             return None
-
-        decoded_base64_auth_header = self.decode_base64_authorization_header(
-                base64_auth_header
-                )
-        if decoded_base64_auth_header is None:
-            return None
-
-        email, password = self.extract_user_credentials(
-                decoded_base64_auth_header
-                )
-        if email is None or password is None:
-            return None
-
-        return self.user_object_from_credentials(email, password)
+        user_email, user_password = self.decode_base64_authorization_header(user_credentials)
+        user = User.search({'email': user_email})  # Fetch the user by email
+        if user is None or not user.is_valid_password(user_password):
+            return None  # User doesn't exist or password is invalid
+        return user
