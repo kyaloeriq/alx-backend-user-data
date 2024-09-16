5719820545c1cf797a65f4c4c9a065bcf5bfc6d0
kyaloeriq <kyaloeriq@gmail.com>
2024-09-16 06:31:20 +0000

    SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 """
-Module for Session Authentication.
+SessionAuth module for managing session IDs.
 """
 
-from api.v1.auth.auth import Auth
-
+import uuid
+from .auth import Auth
 
 class SessionAuth(Auth):
     """
-    Session Authentication class that inherits from Auth.
+    SessionAuth class for managing session-based authentication.
     """
-    pass
+    # Class attribute to store session IDs and associated user IDs
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session ID for a given user_id.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        # Generate a session ID using uuid4
+        session_id = str(uuid.uuid4())
+
+        # Store the session_id with the associated user_id in the dictionary
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Return the generated session ID
+        return session_id
